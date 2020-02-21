33f741a551ab9374d3054bd0cb50939624bbb89b
LegNBass <charmentroutv@gmail.com>
2020-02-21 09:36:05 -0500

    bot responds to a date now

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,12 @@
+import re
 import discord
 
 
 class LiveDead(discord.Client):
+    _message_rules = (
+        (lambda message: re.match(r'\d{4}-\d{2}-\d{2}', message.content), lambda match: f"The date is {match.group()}"),
+    )
+
     def __init__(self, token):
         self.token = token
         super().__init__()
@@ -9,6 +14,14 @@ class LiveDead(discord.Client):
     async def on_ready(self):
         print(f'{self.user} has connected to Discord')
         print(f"Connected to {self.guilds}")
+
+    # Meat 'n taters
+    async def on_message(self, message):
+        if message.author == self.user:
+            return  # Never reply to myself
+        for rule, response in self._message_rules:
+            if result := rule(message):
+                await message.channel.send(response(result))
 
     def run(self):
         super().run(self.token)
